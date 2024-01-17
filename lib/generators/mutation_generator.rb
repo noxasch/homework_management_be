882e86656ae518bcbda1ab7cce3337944d51400d
Alexander Dischberg <noxasch@gmail.com>
2024-01-17 12:07:43 +0800

    feat: add mutation generator

---
+++ lib/generators/mutation_generator.rb
@@ -0,0 +1,18 @@
+class MutationGenerator < Rails::Generators::NamedBase
+  desc 'This generator creates an mutation file inside app/mutations'
+
+  def create_mutation_file
+    create_file "app/mutations/#{file_path}.rb", <<~RUBY
+      class #{class_name}
+        required do
+        end
+
+        protected
+
+        def execute; end
+
+        def validate; end
+      end
+    RUBY
+  end
+end
