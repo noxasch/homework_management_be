143802322be0173a63377970754d2eb0bdf9e135
Alexander Dischberg <noxasch@gmail.com>
2024-01-19 16:33:24 +0800

    chore: fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,14 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+# NOTE: fix this when deploy
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "*"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
